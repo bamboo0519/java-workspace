cb9b108b9dc9aeb958e8d839f24dc8b0536ffc8d
c0907 <chenl0907@dtdream.com>
2017-11-01 20:03:11 +0800

    同步本地

--- project-practice/account/src/main/java/top/ibamboo/account/dao/AccountDao.java
+++ project-practice/account/src/main/java/top/ibamboo/account/dao/AccountDao.java
@@ -1,15 +1,13 @@
 package top.ibamboo.account.dao;
 
 
-import com.dtdream.commons.mybatis.BaseMyBatisDao;
 import org.springframework.stereotype.Repository;
-import top.ibamboo.account.model.Account;
 
 /**
  * Created by bamboo on 2017/8/29.
  */
 @Repository
-public class AccountDao extends BaseMyBatisDao<Account> {
+public class AccountDao {
 
 
 }
